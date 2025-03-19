109de48b5398466606e68de3dd66e5b0f656a98f
GuilhermeD9 <sgsouza.173@gmail.com>
2025-03-19 10:49:08 -0300

    first commit

---
+++ src/main/java/dev/guilherme/CoworClean/CoworCleanApplication.java
@@ -0,0 +1,13 @@
+package dev.guilherme.CoworClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworCleanApplication.class, args);
+	}
+
+}
